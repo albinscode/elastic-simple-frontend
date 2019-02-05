84ce895fff1aabeeb5dc0ab2a760c4eb2e5e5f8b
Albin Vigier <avigier@linagora.com>
2019-02-05 21:48:36 +0100

    exerpt - WIP

--- src/components/search-page/search-page.ts
+++ src/components/search-page/search-page.ts
@@ -27,6 +27,36 @@ export class Result {
         }
         this.content = this.content.substr(0, environment.maxContentSize);
     }
+
+    // // @see https://stackoverflow.com/questions/16159532/find-words-around-search-term-snippet-with-javascript
+    // function getExcerpt(text, searchTerm, precision) {
+    // var words = text.split(" "),
+    //     index = words.firstOccurance(searchTerm),
+    //     result = [], // resulting array that we will join back
+    //     startIndex, stopIndex;
+    // // now we need first <precision> words before and after searchTerm
+    // // we can use slice for this matter
+    // // but we need to know what is our startIndex and stopIndex
+    // // since simple substitution from index could lead us to
+    // // a negative value
+    // // and adding to an index could get us to exceeding words array length
+
+    // startIndex = index - precision;
+    // if (startIndex < 0) {
+    //     startIndex = 0;
+    // }
+
+    // stopIndex = index + precision + 1;
+    // if (stopIndex > words.length) {
+    //     stopIndex = words.length;
+    // }
+
+
+    // result = result.concat( words.slice(startIndex, index) );
+    // result = result.concat( words.slice(index, stopIndex) );
+    // return result.join(' '); // join back
+
+    // }
 }
 
 @Component
